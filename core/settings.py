9dae7bb7f505bd1ea274f0dc49eabc2908fc8a3e
Soft <salisusoftadamu@gmail.com>
2025-12-04 01:19:21 +0100

    Fix Allowed Hosts

--- core/settings.py
+++ core/settings.py
@@ -17,8 +17,12 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-# --- SECURITY CONFIGURATION ---
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1']
+
+# Add Render's internal hostname automatically
+RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
+if RENDER_EXTERNAL_HOSTNAME:
+    ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
 
 # Application definition
 
